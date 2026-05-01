66da04c1c43d821c727c2cdd46d5dc7620c2856d
wellingtonbrito <wellingtondebritosouza@gmail.com>
2026-05-01 17:59:40 -0300

    startando a aplicação react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return(
+    <div className="flex items-center justify-center h-screen">
+       <h1 className="text-4xl">Olá Mundo!</h1>
+    </div>
+  )
+}
+
+export default App
